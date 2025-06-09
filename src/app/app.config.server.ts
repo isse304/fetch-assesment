84f67a979dd84d4a51aa9dbfa64ff965f4f0a326
isse304 <101603508+isse304@users.noreply.github.com>
2025-06-09 16:35:02 -0500

    fixing version issues attempt 7

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,12 +1,10 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(withRoutes(serverRoutes))
-  ]
+  providers: [provideServerRendering(), provideNoopAnimations()],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
